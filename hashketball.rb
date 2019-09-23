e3976a168e086ac30cbc52c25037ad62f7a752ff
Li Xie <li_xie1992@hotmail.com>
2019-09-23 23:12:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -161,6 +161,7 @@ def player_numbers(team_name)
   game_hash.each do |home_or_away, team_data|
     if team_data[:team_name] == team_name
       team_data[:players].each do |player_data|
+        binding.pry
         jersey_numbers_array.push(player_data[:number])
       end
     end 

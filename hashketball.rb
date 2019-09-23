cec65a34addfd2e3feb6a5c9d494bab09df60938
Li Xie <li_xie1992@hotmail.com>
2019-09-23 22:00:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -121,7 +121,6 @@ def game_hash
 end 
 
 def num_points_scored(player_name)
-  score = []
   game_hash.each do |home_or_away, team_data|
     if team_data[:players].include?(player_name)
       return team_data[:players][player_name][:points]

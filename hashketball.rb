bcf861265291d9cf5a6c857bc048434a24bb9f51
Li Xie <li_xie1992@hotmail.com>
2019-09-23 22:03:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,23 +120,23 @@ def game_hash
   }
 end 
 
-# def num_points_scored(player_name)
-#   #  player_name is passed in by rspecs
-#   players = get_players
-#   find_player(players, player_name)[:points]
-# end
+def num_points_scored(player_name)
+  #  player_name is passed in by rspecs
+  players = get_players
+  find_player(players, player_name)[:points]
+end
 
-# def get_players
-#   game_hash.map do |team_info|
-#     team_info[1][:players]
-#   end.flatten
-# end
+def get_players
+  game_hash.map do |team_info|
+    team_info[1][:players]
+  end.flatten
+end
 
-# def find_player(players, player_name)
-#   players.find do |player|
-#     player[:player_name]==player_name
-#   end
-# end
+def find_player(players, player_name)
+  players.find do |player|
+    player[:player_name]==player_name
+  end
+end
 
 # def num_points_scored(player_name)
 #   game_hash.each do |home_or_away, team_data|

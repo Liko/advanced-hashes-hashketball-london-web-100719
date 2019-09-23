8f4d12aa9a237d6cc7dd6538f15d9bc60fb7edef
Li Xie <li_xie1992@hotmail.com>
2019-09-23 23:14:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -160,7 +160,7 @@ def player_numbers(team_name)
   jersey_numbers_array = []
   game_hash.each do |home_or_away, team_data|
     if team_data[:team_name] == team_name
-      team_data[:players].each do |player_data|
+      team_data[:players].each do |player_name, player_data|
         jersey_numbers_array.push(player_data[1][:number])
       end
     end 

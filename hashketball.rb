b548ddaae3c943fb184a671f58e9404dbf802a83
Li Xie <li_xie1992@hotmail.com>
2019-09-23 20:39:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -124,7 +124,7 @@ def num_points_scored(name)
   game_hash.each do |team_data|
     team_data[1][:players].find do |player_name, player_stats|
       if (name == player_name)
-        puts yolo
+        puts name
       end
     end
   end

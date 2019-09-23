feac303274cbade6bb3ebe283816224648752bd2
Li Xie <li_xie1992@hotmail.com>
2019-09-23 23:23:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -114,15 +114,7 @@ def game_hash
           steals: 7,
           blocks: 5,
           slam_dunks: 12
-          
-           :number => 33,
-        :shoe => 15,
-        :points => 6,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 7,
-        :blocks => 5,
-        :slam_dunks => 12
+        
         }
       }
     }
